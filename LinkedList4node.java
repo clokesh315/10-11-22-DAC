df052f1ea0015c8066b8a950b1cf0819bded5702
Lokesh Choudhari <clokesh315@gmail.com>
2023-02-02 15:42:14 +0530

    addlast

---
+++ LinkedList4node.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class LinkedList4node {
+    public static void main(String args[]) {
+        LinkedList lst = new LinkedList();
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+        lst.add(4);
+
+    }
+}
